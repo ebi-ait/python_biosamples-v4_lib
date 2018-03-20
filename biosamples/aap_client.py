fe673ce40a9b64e1094daa5ca78dc5505857fe16
Luca Cherubin <luca.cherubin@gmail.com>
2018-03-20 20:14:09 +0000

    Fixes #1

--- biosamples/aap_client.py
+++ biosamples/aap_client.py
@@ -1,11 +1,20 @@
 import requests
-import biosamples
 
 
-def get_token(username=None, password=None, url=biosamples.AAP_TOKEN_URL):
-    if username is None and password is None:
-        raise Exception("You need to provide username and password to use the AAP client")
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == requests.codes.ok:
-        return response.text
-    return response
+class AapClient:
+    def __init__(self, username=None, password=None, url=None):
+        if username is None:
+            raise Exception("An AAP username has not been provided")
+        if password is None:
+            raise Exception("The password associated with the username is missing ")
+        if url is None:
+            raise Exception("A url to use with the client is missing")
+        self.username = username
+        self.password = password
+        self.url = url
+
+    def get_token(self):
+        response = requests.get(self.url, auth=(self.username, self.password))
+        if response.status_code == requests.codes.ok:
+            return response.text
+        return response
