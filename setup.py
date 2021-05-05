ce7e5a98d5352003b1f0a320a47fcab83d566566
Karoly Erdos <karoly@ebi.ac.uk>
2021-05-05 14:32:24 +0100

    Change the project reference URLS in the setup.py and remove the author field

--- setup.py
+++ setup.py
@@ -18,19 +18,17 @@ here = path.abspath(path.dirname(__file__))
 
 
 setup(
-    name='biosamples_v4',
-    version='1.2.0',
+    name='biosamples-client',
+    version='1.3.0',
     description='A lib to interact with BioSamples-v4 API',
     long_description="A library with functions to wrap the BioSamples API - "
                      "https://wwwdev.ebi.ac.uk/biosamples/docs/guides/submit",
     long_description_content_type='text/markdown',
-    author="Luca Cherubin",
-    author_email="cherubin@ebi.ac.uk",
-    url='https://github.com/Kerruba/python_biosamples-v4_lib',
+    url='https://github.com/ebi-ait/python_biosamples-v4_lib',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     project_urls={
-        'Bug Reports': 'https://github.com/Kerruba/python_biosamples-v4_lib/issues',
-        'Source': 'https://github.com/Kerruba/python_biosamples-v4_lib',
+        'Bug Reports': 'https://github.com/ebi-ait/python_biosamples-v4_lib/issues',
+        'Source': 'https://github.com/ebi-ait/python_biosamples-v4_lib',
     },
     install_requires=['pyjwt', 'requests', 'pytz', 'requests_cache']
 )
